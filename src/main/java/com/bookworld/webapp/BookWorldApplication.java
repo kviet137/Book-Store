935e22599370b01f63e7cf7c6f861dfc6cb80163
kviet137 <kviet137@gmail.com>
2024-12-18 11:48:04 -0500

    start of case study project

---
+++ src/main/java/com/bookworld/webapp/BookWorldApplication.java
@@ -0,0 +1,13 @@
+package com.bookworld.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookworld/webapp/BookWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookworld.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
