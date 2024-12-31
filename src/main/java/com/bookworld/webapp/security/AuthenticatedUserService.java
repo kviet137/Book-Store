a962610da477eb1253d315d30c63b8b16b058026
kviet137 <kviet137@gmail.com>
2024-12-31 18:01:07 -0500

    test

--- src/main/java/com/bookworld/webapp/security/AuthenticatedUserService.java
+++ src/main/java/com/bookworld/webapp/security/AuthenticatedUserService.java
@@ -1,8 +1,6 @@
 package com.bookworld.webapp.security;
 
-<<<<<<< HEAD
-public class AuthenticatedUserService {//55
-=======
-public class AuthenticatedUserService {//66
->>>>>>> main
+
+public class AuthenticatedUserService {//22
+
 }
