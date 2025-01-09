54e72f65e45b30269477ed779c7668818c451974
kviet137 <kviet137@gmail.com>
2025-01-09 18:52:48 -0500

    some custom annotation for validation

---
+++ src/main/java/com/bookworld/webapp/validation/CurrentPasswordValidImpl.java
@@ -0,0 +1,41 @@
+package com.bookworld.webapp.validation;
+
+
+import com.bookworld.webapp.database.entity.User;
+import com.bookworld.webapp.security.AuthenticatedUserService;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class CurrentPasswordValidImpl implements ConstraintValidator<CurrentPasswordValid, String> {
+
+    public static final Logger LOG = LoggerFactory.getLogger(CurrentPasswordValidImpl.class);
+
+    @Autowired
+    private AuthenticatedUserService authenticatedUserService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void initialize(CurrentPasswordValid constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (StringUtils.isEmpty(value)) {
+            return true;
+        }
+
+        User user = authenticatedUserService.loadCurrentUser();
+
+        return passwordEncoder.matches(value, user.getPassword());
+    }
+
+}
